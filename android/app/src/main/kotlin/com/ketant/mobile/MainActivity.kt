830494cfe09de9b5f4c5f8106ff7a6c42a068d42
M Akhyar <62693087+Akhyar41@users.noreply.github.com>
2026-04-15 21:39:37 +0700

    init project

---
+++ android/app/src/main/kotlin/com/ketant/mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ketant.mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/cli/meet_2.dart
@@ -0,0 +1,16 @@
+
+class Mahasiswa {
+  String name;
+  Mahasiswa({
+    required this.name
+  });
+}
+
+void main() {
+
+  List<Mahasiswa> lists = [];
+
+  lists.add(Mahasiswa(name: "Mustofa"));
+  lists.add(Mahasiswa(name: "Akhyar"));
+
+}
